4403a8680ab4027a1c322f2d95cb6bf5c1a5452a
Krish Rawat <46595556+Krishrawat@users.noreply.github.com>
2019-03-27 04:09:44 +0530

    Update simple battleship game.py

--- simple battleship game.py
+++ simple battleship game.py
@@ -19,9 +19,6 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print (ship_row)
-print (ship_col)
-
 for turn in range(4): 
     guess_row = int(input("Guess Row: "))
     guess_col = int(input("Guess Col: "))
@@ -31,9 +28,9 @@ for turn in range(4):
         break
     else:
         if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-        print ("Oops, that's not even in any pool in the ocean.")
+        print ("Oops! that's not even in any pool in the ocean.")
         elif(board[guess_row][guess_col] == "X"):
-        print "You guessed that one already."
+        print ("You guessed that one already.")
     else:
         if turn==3:
             print ("Game Over")
